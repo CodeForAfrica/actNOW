b13202311afd9a95b038ffbc7ea48a201d90a073
esir <esirkings@gmail.com>
2021-06-02 09:58:34 +0300

    Allow blank username on the API

--- actnow/accounts/serializers.py
+++ actnow/accounts/serializers.py
@@ -2,16 +2,13 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
-from .utils import email_address_exists
+from .utils import email_address_exists, username_exists
 
 User = get_user_model()
 
 
 class RegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(
-        max_length=10,
-        required=False,
-    )
+    username = serializers.CharField(max_length=10, required=False, allow_blank=True)
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True)
 
@@ -22,14 +19,24 @@ class RegisterSerializer(serializers.Serializer):
             )
         return email
 
+    def validate_username(self, username):
+        if username_exists(username):
+            raise serializers.ValidationError(
+                _("A user is already registered with this username"),
+            )
+        return username
+
     def get_cleaned_data(self):
         return {
-            "username": self.validated_data.get("username", ""),
-            "password": self.validated_data.get("password", ""),
-            "email": self.validated_data.get("email", ""),
+            "username": self.validated_data.get("username"),
+            "password": self.validated_data.get("password"),
+            "email": self.validated_data.get("email"),
         }
 
     def save(self, request):
         cleaned_data = self.get_cleaned_data()
+        if not cleaned_data["username"]:
+            del cleaned_data["username"]
+
         user = User.objects.create_user(**cleaned_data)
         return user
--- actnow/accounts/utils.py
+++ actnow/accounts/utils.py
@@ -5,3 +5,9 @@ User = get_user_model()
 
 def email_address_exists(email):
     return User.objects.filter(email=email).first()
+
+
+def username_exists(username):
+    if not username:
+        return None
+    return User.objects.filter(username=username).first()
