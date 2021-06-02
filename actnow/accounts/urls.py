d246a762e91a31d4fd5f557529c9105127e67771
esir <esirkings@gmail.com>
2021-06-02 12:43:16 +0300

    Use Restful URLs

--- actnow/accounts/urls.py
+++ actnow/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import RegisterView
+from .views import UserRegistrationView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="account_register"),
+    path("", UserRegistrationView.as_view(), name="accounts-user_registration"),
 ]
