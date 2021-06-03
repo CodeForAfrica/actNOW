090340256cd3928dbb5151db29c39b0bcb96643f
esir <esirkings@gmail.com>
2021-06-03 15:01:40 +0300

    Use django default admin site to prevent having to manually register all models from 3rd party packages.

--- actnow/settings.py
+++ actnow/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "actnow.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [Path.joinpath(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
