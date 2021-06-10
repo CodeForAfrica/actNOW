e9bb587e9e7cc93091fbb95c9832dd7fe686a231
esir <esirkings@gmail.com>
2021-06-10 05:41:21 +0300

    Use nginx to serve media files

--- actnow/urls.py
+++ actnow/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,6 +21,3 @@ urlpatterns = [
     path("accounts/", include("actnow.accounts.urls")),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
