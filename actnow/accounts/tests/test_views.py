7035f1d6e47109da7af7c719a32533fbcbe24fe0
esir <esirkings@gmail.com>
2021-06-15 16:20:31 +0300

    Update tests to use application token

--- actnow/accounts/tests/test_views.py
+++ actnow/accounts/tests/test_views.py
@@ -1,16 +1,26 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls.base import reverse
+from django.utils import timezone
+from oauth2_provider.models import get_application_model, get_id_token_model
 
 User = get_user_model()
 
 
 class TestUserRegistrationView(TestCase):
     def setUp(self):
+        self.application = get_application_model()()
+        self.application.save()
+        expires = timezone.now() + timezone.timedelta(seconds=10)
+        self.id_token = get_id_token_model()(
+            application=self.application, expires=expires
+        )
+        self.id_token.save()
         self.user_data = {
             "username": "TestUser",
             "email": "testuser@mail.com",
             "password": "RandomPassword#2323#",
+            "application_token": str(self.id_token.jti),
         }
         self.url = reverse("accounts-user_registration")
 
@@ -41,3 +51,9 @@ class TestUserRegistrationView(TestCase):
         )
         self.assertEqual(400, response.status_code)
         self.assertEqual(1, User.objects.count())
+
+    def test_only_apps_can_register_a_user(self):
+        user_data = {**self.user_data}
+        del user_data["application_token"]
+        response = self.client.post(self.url, user_data)
+        self.assertEqual(401, response.status_code)
