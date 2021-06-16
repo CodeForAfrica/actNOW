f616426460960b2560a284acfc0301a3739682e7
esir <esirkings@gmail.com>
2021-06-16 12:31:10 +0300

    Allow users to obtain tokens using username and password

--- actnow/accounts/urls.py
+++ actnow/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
 from .views import UserRegistrationView
 
 urlpatterns = [
     path("", UserRegistrationView.as_view(), name="accounts-user_registration"),
+    path("api-token-auth/", views.obtain_auth_token),
 ]
