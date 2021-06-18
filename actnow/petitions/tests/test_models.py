ed44b634ff082a1238a6f4f6e6ae3f164535f9f5
khadija <khadijamahanga@y7mail.com>
2021-06-18 10:13:37 +0300

    remove unique test

--- actnow/petitions/tests/test_models.py
+++ actnow/petitions/tests/test_models.py
@@ -1,4 +1,3 @@
-from django.db.utils import IntegrityError
 from django.test import TestCase
 
 from ..models import Petition
@@ -14,18 +13,3 @@ class PetitionTest(TestCase):
         )
         self.assertEqual(1, Petition.objects.count())
 
-    def test_petition_is_unique(self):
-        Petition.objects.create(
-            title="Petition B",
-            problem_statement="Petitioning Issue",
-            description="Description",
-            recipients="Member of Parliament",
-        )
-
-        with self.assertRaises(IntegrityError):
-            Petition.objects.create(
-                title="Petition B",
-                problem_statement="Problem Statement",
-                description="Details of Petition",
-                recipients="City Council",
-            )
