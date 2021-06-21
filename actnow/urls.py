df2dafd8f96bdd1b1ca69670ac8343f65f6ebee5
esir <esirkings@gmail.com>
2021-06-21 11:54:01 +0300

    Adds UserProfileView api endpoint

--- actnow/urls.py
+++ actnow/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("actnow.accounts.urls")),
+    path("user-profiles/", include("actnow.profiles.urls")),
     path("petitions/", include("actnow.petitions.urls")),
     path("profiles/", include("actnow.profiles.urls")),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
