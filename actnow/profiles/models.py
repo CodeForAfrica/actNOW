4dc65dc6287c4634cd069bf5b6db57f960961176
esir <esirkings@gmail.com>
2021-07-01 16:00:00 +0300

    Make all user profile fields Optional other than first_name

--- actnow/profiles/models.py
+++ actnow/profiles/models.py
@@ -11,13 +11,13 @@ from .validators import validate_social_media_link
 class UserProfile(TimestampedModelMixin):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     first_name = models.CharField(_("first name"), max_length=255)
-    last_name = models.CharField(_("last name"), max_length=255)
-    bio = models.TextField(_("about me"), max_length=255, blank=True)
-    photo = models.ImageField(_("photo"), blank=True)
-    location = models.TextField(_("location"), max_length=255, blank=True)
-    phone_number = PhoneNumberField(_("phone number"), blank=True)
+    last_name = models.CharField(_("last name"), max_length=255, null=True, blank=True)
+    bio = models.TextField(_("about me"), max_length=255, blank=True, null=True)
+    photo = models.ImageField(_("photo"), blank=True, null=True)
+    location = models.TextField(_("location"), max_length=255, blank=True, null=True)
+    phone_number = PhoneNumberField(_("phone number"), blank=True, null=True)
     social_media_link = models.URLField(
-        validators=[validate_social_media_link], blank=True
+        validators=[validate_social_media_link], blank=True, null=True
     )
 
     def __str__(self):
