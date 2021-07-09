7ec49a09c2749e921a677e0f2da26d304b728bf2
esir <esirkings@gmail.com>
2021-07-09 13:58:35 +0300

    Undo API versioning changes

--- actnow/urls.py
+++ actnow/urls.py
@@ -18,9 +18,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("v1/accounts/", include("actnow.accounts.urls")),
-    path("v1/", include("actnow.petitions.urls")),
-    path("v1/profiles/", include("actnow.profiles.urls")),
+    path("accounts/", include("actnow.accounts.urls")),
+    path("petitions/", include("actnow.petitions.urls")),
+    path("profiles/", include("actnow.profiles.urls")),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
     path("", include("actnow.site.urls")),
 ]
