833250aba2700b6e2ebb5257d32fefe7a89ef4ff
esir <esirkings@gmail.com>
2021-07-13 11:13:16 +0300

    Adds api endpoint for following a pettition

--- actnow/petitions/urls.py
+++ actnow/petitions/urls.py
@@ -1,9 +1,11 @@
+from django.urls import path
 from rest_framework import routers
 
-from .views import PetitionView, SignatureView
+from .views import PetitionView, SignatureView, follow_petition
 
 router = routers.DefaultRouter()
 router.register(r"petitions", PetitionView)
 router.register(r"signatures", SignatureView)
 
 urlpatterns = router.urls
+urlpatterns += [path("<int:pk>/follow", follow_petition, name="follow_pettition")]
--- actnow/petitions/views.py
+++ actnow/petitions/views.py
@@ -1,4 +1,14 @@
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Petition, Signature
 from .serializers import PetitionSerializer, SignatureSerializer
@@ -18,3 +28,17 @@ class SignatureView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(signatory=self.request.user)
+
+
+@api_view(["GET", "POST"])
+@authentication_classes(
+    [SessionAuthentication, TokenAuthentication, OAuth2Authentication]
+)
+@permission_classes([IsAuthenticated])
+def follow_petition(request, pk):
+    petition = get_object_or_404(Petition, pk=pk)
+    if request.method == "POST":
+        petition.followers.add(request.user)
+        petition.save()
+    petition_serializer = PetitionSerializer(petition)
+    return Response(data=petition_serializer.data)
