cb99ece28c186746c3e7214201c7b43ebfb73857
_ Kilemensi <clemence@codeforafrica.org>
2021-08-02 22:35:34 +0300

    Update read serializers

--- actnow/profiles/serializers.py
+++ actnow/profiles/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
 
+from actnow.accounts.serializers import UserSerializer
+
 from .models import OrganisationProfile, UserProfile
 
 
 class OrganisationProfileSerializer(serializers.ModelSerializer):
+    owners = UserSerializer(read_only=True, many=True)
+
     class Meta:
         model = OrganisationProfile
         fields = "__all__"
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        if not rep.get("name"):
+            rep["name"] = str(instance)
+
+        return rep
+
     class Meta:
         model = UserProfile
         fields = "__all__"
