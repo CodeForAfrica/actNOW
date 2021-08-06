305a04b1a6b35753a21fc0169b91a4ae8724c0b5
_ Kilemensi <clemence@codeforafrica.org>
2021-08-06 18:37:03 +0300

    Tie url and view for /profiles

--- actnow/profiles/urls.py
+++ actnow/profiles/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import OrganisationProfileView, UserProfileView
+from .views import OrganisationProfileView, ProfileView, UserProfileView
 
 router = DefaultRouter()
 router.register(
     r"organisations", OrganisationProfileView, basename="organisation_profile"
 )
 router.register(r"users", UserProfileView, basename="user_profile")
+router.register(r"", ProfileView)
 
 urlpatterns = router.urls
