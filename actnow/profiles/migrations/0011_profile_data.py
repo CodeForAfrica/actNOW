6df933342165f379ada3b63914e504f66056edc6
_ Kilemensi <clemence@codeforafrica.org>
2021-08-06 18:36:17 +0300

    Prepare DB

---
+++ actnow/profiles/migrations/0011_profile_data.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+
+
+def link_profiles(apps, schema_editor):
+    Profile = apps.get_model("profiles", "Profile")
+    OrganisationProfile = apps.get_model("profiles", "OrganisationProfile")
+    UserProfile = apps.get_model("profiles", "UserProfile")
+
+    for organisation in OrganisationProfile.objects.all():
+        profile = Profile(organisation_profile=organisation)
+        profile.save()
+
+    for user in UserProfile.objects.all():
+        profile = Profile(user_profile=user)
+        profile.save()
+
+
+def unlink_profiles(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("profiles", "0010_profile"),
+    ]
+
+    operations = [
+        migrations.RunPython(link_profiles, unlink_profiles),
+    ]
