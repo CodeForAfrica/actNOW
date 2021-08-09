d4a95b12f79f0c5faa29e398b88c9a2695df3cd9
_ Kilemensi <clemence@codeforafrica.org>
2021-08-09 13:54:56 +0300

    Add tests for Profile creation

--- actnow/profiles/tests/test_models.py
+++ actnow/profiles/tests/test_models.py
@@ -14,7 +14,12 @@ class UserProfileTest(TestCase):
         User.objects.create_user(
             email="root@root.com", password="RandomPassword321", username="root"
         )
-        self.assertEqual(1, models.UserProfile.objects.count())
+        user_profiles = models.UserProfile.objects.all()
+        profiles = models.Profile.objects.all()
+        self.assertEqual(1, len(user_profiles))
+        self.assertEqual(1, len(profiles))
+        self.assertEqual(user_profiles[0], profiles[0].user_profile)
+        self.assertIsNone(profiles[0].organisation_profile)
 
     def test_validate_social_media_link(self):
         user = UserFactory()
@@ -30,7 +35,12 @@ class UserProfileTest(TestCase):
 class OrganisationProfileTestCase(TestCase):
     def test_create_organisation_profile(self):
         OrganisationProfileFactory()
-        self.assertEqual(1, models.OrganisationProfile.objects.count())
+        org_profiles = models.OrganisationProfile.objects.all()
+        profiles = models.Profile.objects.all()
+        self.assertEqual(1, len(org_profiles))
+        self.assertEqual(1, len(profiles))
+        self.assertEqual(org_profiles[0], profiles[0].organisation_profile)
+        self.assertIsNone(profiles[0].user_profile)
 
     def test_organisation_can_not_have_more_than_2_owners(self):
         org = OrganisationProfileFactory()
