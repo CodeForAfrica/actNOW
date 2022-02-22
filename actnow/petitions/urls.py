5516d293b66267591dccfe0ec365bc52ac54a9ae
_ Kilemensi <clemence@codeforafrica.org>
2022-02-22 15:47:28 +0300

    Use separated views and viewsets

--- actnow/petitions/urls.py
+++ actnow/petitions/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework import routers
 
-from .views import PetitionView, SignatureView, follow_petition
+from .views import follow_petition
+from .viewsets import PetitionViewSet, SignatureViewSet
 
 router = routers.DefaultRouter()
-router.register(r"signatures", SignatureView)
-router.register(r"", PetitionView, basename="petitions")
+router.register(r"signatures", SignatureViewSet)
+router.register(r"", PetitionViewSet, basename="petitions")
 
 urlpatterns = router.urls
 urlpatterns += [path("<int:pk>/followers", follow_petition, name="petition_followers")]
