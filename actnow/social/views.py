b4234020d33223e2504a7e7a95559a71091d7a1f
esir <esirkings@gmail.com>
2022-02-25 11:27:05 +0300

    Adds social app for holding all OAuth social apps e.g Google

---
+++ actnow/social/views.py
@@ -0,0 +1,9 @@
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://localhost:8000/api/social/google/login/callback/"
+    client_class = OAuth2Client
